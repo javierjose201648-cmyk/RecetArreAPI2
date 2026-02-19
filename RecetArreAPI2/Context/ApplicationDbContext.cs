be74343d842b0c4e98eb8f3ff44949280744f314
javierjose201648-cmyk <javierjose201648@gmail.com>
2026-02-19 10:24:44 -0600

    Agregar archivos de proyecto.

---
+++ RecetArreAPI2/Context/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RecetArreAPI2.Models;
+
+namespace RecetArreAPI2.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configuración de Categoria
+            builder.Entity<Categoria>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Descripcion)
+                    .HasMaxLength(500)
+                    .IsRequired(false);
+
+                entity.Property(e => e.CreadoUtc)
+                    .IsRequired()
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                // Relación con ApplicationUser
+                entity.HasOne(e => e.CreadoPorUsuario)
+                    .WithMany()
+                    .HasForeignKey(e => e.CreadoPorUsuarioId)
+                    .OnDelete(DeleteBehavior.SetNull)
+                    .IsRequired(false);
+
+                // Índices
+                entity.HasIndex(e => e.Nombre).IsUnique();
+                entity.HasIndex(e => e.CreadoPorUsuarioId);
+            });
+        }
+    }
+}
